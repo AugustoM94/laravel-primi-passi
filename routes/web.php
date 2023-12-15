7ba493a130c13037809c35bb0bc489b08131bf6a
AugustoM94 <email@github.com>
2023-12-15 17:05:55 +0100

    project completed

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/laravel', function () {
+    $data = [
+        'title' => 'Laravel',
+        'image' => 'https://media.licdn.com/dms/image/D5612AQGrUk-Euq6J_w/article-cover_image-shrink_600_2000/0/1677780279349?e=2147483647&v=beta&t=OJ7_EgucF9GJPnzVn23mbaXtKdrf-kvDffVHGC-h38w',
+        'text' => 'Laravel è un framework open source di tipo MVC scritto in PHP per lo sviluppo di applicazioni web, creato nel 2011 da Taylor Otwell come derivazione di Symfony.',
+    ];
+
+    return view('laravel', $data);
+})->name('laravel');
